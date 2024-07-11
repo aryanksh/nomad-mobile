10901481dcf2177843ea6b489fbfbc77f7f26349
aryanksh <aryanksh@gmail.com>
2024-07-11 16:37:02 -0400

    implementing google maps api. loads apple maps, error when loading google maps

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,12 @@
-import { Text, View, StyleSheet } from "react-native";
+import React, { useState, useEffect, forwardRef } from 'react';
+import { Text, View, StyleSheet, FlatList } from "react-native";
 import { Stack } from 'expo-router';
+import axios from 'axios';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
+// const MapViewWithRef = React.forwardRef((props, ref) => (
+//   <MapView ref={ref} {...props} />
+// ));
 
 export default function Index() {
   return (
@@ -8,6 +14,8 @@ export default function Index() {
       <Stack.Screen options={{ title: 'Nomad' }} />
       <View style={styles.container}>
         <Text>Home page</Text>
+        <MapView style={styles.map} provider={PROVIDER_GOOGLE}/>
+        {/* <MapViewWithRef style={styles.map} provider={PROVIDER_GOOGLE} /> */}
       </View>
     </>
   );
@@ -15,8 +23,13 @@ export default function Index() {
 
 const styles = StyleSheet.create({
   container: {
+    // marginBottom:270,
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
+  map: {
+    height: '100%',
+    width: '100%'
+  }
 });
